ff1862f05eba02f212430d63033b9389b42eccc5
Pradyumna <2022mt03596@wilp.bits-pilani.ac.in>
2024-12-07 17:46:39 +0530

    refactored for urls

--- web/src/Helpers/allUrl.js
+++ web/src/Helpers/allUrl.js
@@ -7,3 +7,14 @@ export const AllUrl = {
   roadsideAssistanceUrl: "http://127.0.0.1:8005",
   feedbackServiceUrl: "http://127.0.0.1:8006",
 };
+
+
+// export const AllUrl = {
+//   authServiceUrl: "http://auth-svc",
+//   customerServiceUrl: "http://customer-svc",
+//   vehicleServiceUrl: "http://vehicle-svc",
+//   bookingServiceUrl: "http://booking-svc",
+//   supportServiceUrl: "http://support-svc",
+//   roadsideAssistanceUrl: "http://rsa-svc",
+//   feedbackServiceUrl: "http://feedback-svc",
+// };

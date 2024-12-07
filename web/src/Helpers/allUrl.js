3e27e65deaf22b0ae717568ad45d19cb21371375
Pradyumna <2022mt03596@wilp.bits-pilani.ac.in>
2024-12-07 17:59:33 +0530

    refactored for urls

--- web/src/Helpers/allUrl.js
+++ web/src/Helpers/allUrl.js
@@ -1,20 +1,11 @@
+const BASE_IP =  "127.0.0.1";
+
 export const AllUrl = {
-  authServiceUrl: "http://127.0.0.1:8001",
-  customerServiceUrl: "http://127.0.0.1:8007",
-  vehicleServiceUrl: "http://127.0.0.1:8002",
-  bookingServiceUrl: "http://127.0.0.1:8003",
-  supportServiceUrl: "http://127.0.0.1:8004",
-  roadsideAssistanceUrl: "http://127.0.0.1:8005",
-  feedbackServiceUrl: "http://127.0.0.1:8006",
+  authServiceUrl: `http://${BASE_IP}:8001`,
+  customerServiceUrl: `http://${BASE_IP}:8007`,
+  vehicleServiceUrl: `http://${BASE_IP}:8002`,
+  bookingServiceUrl: `http://${BASE_IP}:8003`,
+  supportServiceUrl: `http://${BASE_IP}:8004`,
+  roadsideAssistanceUrl: `http://${BASE_IP}:8005`,
+  feedbackServiceUrl: `http://${BASE_IP}:8006`,
 };
-
-
-// export const AllUrl = {
-//   authServiceUrl: "http://auth-svc",
-//   customerServiceUrl: "http://customer-svc",
-//   vehicleServiceUrl: "http://vehicle-svc",
-//   bookingServiceUrl: "http://booking-svc",
-//   supportServiceUrl: "http://support-svc",
-//   roadsideAssistanceUrl: "http://rsa-svc",
-//   feedbackServiceUrl: "http://feedback-svc",
-// };
